dcc52aacc3d5c12a1e580f8ba61971e060b01dcd
DsCodeStudio <DSCodeStudio@gmail.com>
2021-04-12 16:29:10 +0800

    ⚠️ 移除DeepCode 代码分析工具

--- app.js
+++ app.js
@@ -3,70 +3,70 @@
  * @create: 2021-03-23 18:32 PM
  * @license: MIT
  * @lastAuthor: DSCode
- * @lastEditTime: 2021-04-06 15:26 PM
+ * @lastEditTime: 2021-04-12 10:23 AM
  * @desc: 服务器主文件
  */
-"use strict";
+'use strict';
 
 // 引用 Hapi
-const Hapi = require("@hapi/hapi");
+const Hapi = require('@hapi/hapi');
 
-const Inert = require("@hapi/inert");
-const Vision = require("@hapi/vision");
-const HapiSwagger = require("hapi-swagger");
-const Pack = require("./package");
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
 
 // 导入路由
-const Routers = require("./routers/router");
+const Routers = require('./routers/router');
 // 创建一个 HTTP 服务
 // (async () => {})(); 可用此方法改进函数
 const server = Hapi.server({
-  port: 3003,
-  host: "127.0.0.1",
+  port: 3000,
+  host: '192.168.2.110',
   debug: false, // disable Hapi debug console logging
   // 配置跨域
   routes: {
     cors: {
-      origin: ["*"]
-    }
-  }
+      origin: ['*'],
+    },
+  },
 });
 
 // 配置 Swagger
 const swaggerOptions = {
   info: {
-    title: "Test API Documentation",
-    version: Pack.version
-  }
+    title: 'Test API Documentation',
+    version: Pack.version,
+  },
 };
 
 // 初始化 App
 const init = async () => {
   // 使用 Hapi 日志组件，实时监控程序运行情况
   await server.register({
-    plugin: require("hapi-pino"),
+    plugin: require('hapi-pino'),
     options: {
       // 格式化日志输出
       prettyPrint: true,
       redact: [
-        "req.headers.authorization",
-        "key",
-        "path.to.key",
-        "stuff.thats[*].secret"
+        'req.headers.authorization',
+        'key',
+        'path.to.key',
+        'stuff.thats[*].secret',
       ],
-      logEvents: ["server-start", "server-stop", "response", "request-error"] // Default
-    }
+      logEvents: ['server-start', 'server-stop', 'response', 'request-error'], // Default
+    },
   });
 
   // 连接数据库
   await server.register({
-    plugin: require("hapi-plugin-mysql"),
+    plugin: require('hapi-plugin-mysql'),
     options: {
-      host: "localhost",
-      user: "root",
-      password: "ds",
-      port: 3306
-    }
+      host: 'localhost',
+      user: 'root',
+      password: 'ds',
+      port: 3306,
+    },
   });
 
   // 注册 Swagger 组件
@@ -75,13 +75,13 @@ const init = async () => {
     Vision,
     {
       plugin: HapiSwagger,
-      options: swaggerOptions
-    }
+      options: swaggerOptions,
+    },
   ]);
 
   try {
     await server.start();
-    console.log("Server running at:", server.info.uri);
+    console.log('Server running at:', server.info.uri);
   } catch (err) {
     console.log(err);
   }
@@ -91,21 +91,21 @@ const init = async () => {
 };
 
 // 捕获软件运行异常
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
 
-server.events.on("response", function (request) {
+server.events.on('response', function (request) {
   console.log(
     request.info.remoteAddress +
-      ": " +
+      ': ' +
       request.method.toUpperCase() +
-      " " +
+      ' ' +
       request.path +
-      " --> " +
+      ' --> ' +
       request.response.statusCode +
-      " " +
+      ' ' +
       JSON.stringify(request.payload)
   );
 });
